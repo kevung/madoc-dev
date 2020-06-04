2cc4db7c4b9437e1b7f970dc320b69e6c36574ab
Kevin Unger <kevin.unger@protonmail.com>
2020-06-05 00:22:41 +0200

    drone.yml: aspell, impose utilisation bash

--- aspell/check.sh
+++ aspell/check.sh
@@ -1,15 +1,13 @@
+OUTPUT=mispelled.txt
 LANG=$(cat ./aspell/lang.txt)
-echo ${LANG}
-JARGON_COMMON=./aspell/jargon_common.txt
 #jargon pour langue specific
 if [ ${LANG} == 'en' ]; then
   JARGON_LANG=./aspell/jargon_en.txt 
 elif [ ${LANG} == 'fr' ]; then
   JARGON_LANG=./aspell/jargon_fr.txt
 fi
-# JARGON_LANG=./aspell/jargon_fr.txt
+JARGON_COMMON=./aspell/jargon_common.txt
 cat ${JARGON_LANG} ${JARGON_COMMON} > jargon_tmp.txt
-OUTPUT=mispelled.txt
 rm -f ${OUTPUT} && touch ${OUTPUT};
 for f in `find ./docs -iname "*.md"`; do echo $f: $(cat $f | aspell -l ${LANG} --personal=./jargon_tmp.txt list) >> ${OUTPUT}; done;
 rm jargon_tmp.txt
