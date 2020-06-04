9b7c19442acda5a2860d5394e44d23cb41cc809e
Kevin Unger <kevin.unger@protonmail.com>
2020-06-04 22:45:30 +0200

    supprime test jargon

--- aspell/check.sh
+++ aspell/check.sh
@@ -1,11 +1,12 @@
 LANG=$(cat ./aspell/lang.txt)
 JARGON_COMMON=./aspell/jargon_common.txt
 #jargon pour langue specific
-if [ ${LANG} == "en" ]; then
-  JARGON_LANG=./aspell/jargon_en.txt 
-elif [ ${LANG} == "fr" ]; then
-  JARGON_LANG=./aspell/jargon_fr.txt
-fi
+# if [ ${LANG} == "en" ]; then
+#   JARGON_LANG=./aspell/jargon_en.txt 
+# elif [ ${LANG} == "fr" ]; then
+#   JARGON_LANG=./aspell/jargon_fr.txt
+# fi
+JARGON_LANG=./aspell/jargon_fr.txt
 cat ${JARGON_LANG} ${JARGON_COMMON} > jargon_tmp.txt
 OUTPUT=mispelled.txt
 rm -f ${OUTPUT} && touch ${OUTPUT};
