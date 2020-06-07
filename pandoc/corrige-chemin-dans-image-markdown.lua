055ed82da562dbafc9593e6099908377f7e01ae7
Kevin Unger <kevin.unger@protonmail.com>
2020-06-07 15:39:15 +0200

    supprime aspell, pandoc, scripts qui sont traités par madoc-aspell, madoc-pandoc...

--- pandoc/corrige-chemin-dans-image-markdown.lua
+++
@@ -1,9 +0,0 @@
--- permet de corriger le chemin d'une image markdown
--- en effet, avec pandoc, les liens sont relatifs au dossier
--- où est lancee la commande pandoc, et non par rapport au fichier
--- markdown
-
-function Image (img)
-  img.src = img.src:gsub('../images/(.+)', './images/%1')
-  return img
-end
